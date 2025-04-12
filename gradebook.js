51c7249f8295651b6370b1c18a3b7cde207470ba
darobert-waketech <darobert@my.waketech.edu>
2025-04-11 23:17:30 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+  
+}
+
+
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+  
+}
+
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+
